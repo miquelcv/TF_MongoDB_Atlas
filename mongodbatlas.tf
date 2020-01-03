2516722d1342f8d02bc283504ec335ae742c20e4
Miquel Camps <mcampsv@gmail.com>
2020-01-03 13:29:21 +0100

    Update mongodbatlas.tf

--- mongodbatlas.tf
+++ mongodbatlas.tf
@@ -2,6 +2,7 @@ provider "mongodbatlas" {
   public_key = "${var.mongodbatlas_public_key}"
   private_key  = "${var.mongodbatlas_private_key}"
 }
+
 resource "mongodbatlas_project_ip_whitelist" "test" {
     project_id = 5e0f2eabc56c98607861b421
   
@@ -21,4 +22,4 @@ resource "mongodbatlas_project_ip_whitelist" "test" {
       ip_address = "4.5.6.7"
       comment    = "ip address for tf acc testing"
     }
- }
+}
